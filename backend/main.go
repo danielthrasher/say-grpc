42ffb7f45536cf3afc9acae4f1150e56b734e6b1
Daniel Thrasher <dannythrasher@gmail.com>
2020-06-26 11:17:27 -0500

    deployed say command to gke

--- backend/main.go
+++ backend/main.go
@@ -3,26 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os/exec"
 
-	"github.com/Sirupsen/logrus"
+	pb "github.com/danielthrasher/say-grpc/api"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	port := flag.Int("p", 8080, "port to listen to")
 	flag.Parse()
 
-	net.Listen("tcp", fmt.Sprintf(":%d", port))
+	logrus.Infof("listening to port %d", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		logrus.Fatalf("cound not listen to port %d: %v", *port, err)
 	}
 
 	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("could not server: %v", err)
+	}
 }
 
-//cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-//cmd.Stdout = os.Stdout
-//cmd.Stderr = os.Stderr
-//if err := cmd.Run(); err != nil {
-//	log.Fatal(err)
-//}
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("cound not create tmp file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not read tmp file: %v", err)
+	}
+	return &pb.Speech{Audio: data}, nil
+}
